3eef8a78407d584f666c5c9cbc3a6bb631ee7221
SENE-Dev <71398525+SENE-Dev@users.noreply.github.com>
2025-07-15 21:58:08 +0000

    Seeder pour créer l'utilisateur admin

--- config/filesystems.php
+++ config/filesystems.php
@@ -2,31 +2,7 @@
 
 return [
 
-    /*
-    |--------------------------------------------------------------------------
-    | Default Filesystem Disk
-    |--------------------------------------------------------------------------
-    |
-    | Here you may specify the default filesystem disk that should be used
-    | by the framework. The "local" disk, as well as a variety of cloud
-    | based disks are available to your application for file storage.
-    |
-    */
-
     'default' => env('FILESYSTEM_DISK', 'public'),
-
-    /*
-    |--------------------------------------------------------------------------
-    | Filesystem Disks
-    |--------------------------------------------------------------------------
-    |
-    | Below you may configure as many filesystem disks as necessary, and you
-    | may even configure multiple disks for the same driver. Examples for
-    | most supported storage drivers are configured here for reference.
-    |
-    | Supported drivers: "local", "ftp", "sftp", "s3"
-    |
-    */
 
     'disks' => [
 
@@ -40,7 +16,8 @@ return [
 
         'public' => [
             'driver' => 'local',
-            'root' => storage_path('app/public'),
+            // 🔽 Utilise le chemin personnalisé pour Render OU fallback local
+            'root' => env('FILESYSTEM_ROOT', storage_path('app/public')),
             'url' => env('APP_URL').'/storage',
             'visibility' => 'public',
             'throw' => false,
@@ -62,19 +39,9 @@ return [
 
     ],
 
-    /*
-    |--------------------------------------------------------------------------
-    | Symbolic Links
-    |--------------------------------------------------------------------------
-    |
-    | Here you may configure the symbolic links that will be created when the
-    | `storage:link` Artisan command is executed. The array keys should be
-    | the locations of the links and the values should be their targets.
-    |
-    */
-
     'links' => [
-        public_path('storage') => storage_path('app/public'),
+        // Ce lien sera créé par `php artisan storage:link`
+        public_path('storage') => env('FILESYSTEM_ROOT', storage_path('app/public')),
     ],
 
 ];
